d9dadf06805e6ff3427aa75acf259cd175887197
bugattiloh <bugattiloh2000@gmail.com>
2020-10-28 13:07:28 +0300

    Добавьте файлы проекта.

---
+++ EmployeesDB/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using EmployeesDB.Data.Models;
+
+namespace EmployeesDB
+{
+    class Program
+    {
+        static private EmployeesContext _context = new EmployeesContext();
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetEmployeesInformation());
+        }
+
+        static string GetEmployeesInformation()
+        {
+            var employees = _context.Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle
+                })
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
